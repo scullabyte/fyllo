8f67d63bbc2611fa69de39d0410e90e19d2bc6c4
Kevin Scully <scullabyte@gmail.com>
2021-10-20 10:41:24 -0500

    feat: Sets up redux/bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const store = createStore(rootReducer)
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, document.getElementById('root'));
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();

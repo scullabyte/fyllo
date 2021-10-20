4be911dcb1b2a20e8b864fd65c9154b9fcf6299d
Kevin Scully <scullabyte@gmail.com>
2021-10-20 16:31:25 -0500

    feat: Adds bootstrapped layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GuessBox from './components/GuessBox'
+import {Container, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Container fluid>
+      <Row>
+    <Col/>
+    <Col xs={6}>
+    <GuessBox/>
+    </Col>
+    <Col/>
+        </Row>
+      </Container>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const store = createStore(rootReducer)
+import {createStore} from 'redux'
+import mainReducer from './reducers/mainReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(mainReducer)
 
 ReactDOM.render(
     <Provider store={store}>
